d007031ab3ca5851f7a00a49a1c5b7bb9dbd9136
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-11-04 01:14:00 +0900

    :hammer: りんごの3Dレイアウトサンプルを追加

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export const Hero = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full z-50">
+      <div className="mt-[5vh] ml-[5vw]">
+        <h1 className="font-black text-red-500 text-[2.2rem] lg:text-[4.2rem] tracking-tighter overflow-hidden">
+          りんご
+        </h1>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/apple3D/index.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { Canvas, useFrame } from '@react-three/fiber'
+import {
+  Environment,
+  Center,
+  Lightformer,
+  Shadow,
+} from '@react-three/drei'
+import { ReactNode, useRef, useState } from 'react'
+import { Model } from './Apple'
+
+type Props = {
+  position?: [number, number, number]
+  fov?: number
+}
+
+export const Apple3D = ({ position = [0, 1, 2.5], fov = 7 }: Props) => {
+  const [perfSucks, degrade] = useState(true)
+
+  return (
+    <Canvas
+      shadows
+      camera={{ position, fov }}
+      gl={{ preserveDrawingBuffer: true }}
+      eventPrefix="client"
+    >
+      <Env perfSucks={perfSucks} />
+      <CameraRig>
+        <Center>
+          <Model scale={0.5} />
+          <Shadow
+            rotation={[-Math.PI / 2, 0, 0]}
+            scale={0.2}
+            position={[0, 0, 0]}
+            color={'black'}
+            opacity={0.9}
+          />
+        </Center>
+      </CameraRig>
+    </Canvas>
+  )
+}
+
+type RigType = {
+  children: ReactNode
+}
+
+function CameraRig({ children }: RigType) {
+  const groupRef = useRef<THREE.Group>(null)
+  useFrame((state, delta) => {
+    if (groupRef.current) {
+      const rotationY = (Math.sin(state.clock.elapsedTime * 0.8) * Math.PI) / 2
+      groupRef.current.rotation.y = rotationY
+    }
+  })
+  return <group ref={groupRef}>{children}</group>
+}
+
+type EnvProps = {
+  perfSucks: boolean
+}
+
+function Env({ perfSucks }: EnvProps) {
+  return (
+    <Environment
+      frames={perfSucks ? 1 : Infinity}
+      preset="sunset"
+      resolution={256}
+      background
+      blur={0.8}
+    >
+      <Lightformer
+        intensity={9}
+        form="ring"
+        color="#FF624F"
+        rotation-y={Math.PI / 2}
+        position={[-5, 2, -1]}
+        scale={[10, 10, 1]}
+      />
+    </Environment>
+  )
+}
