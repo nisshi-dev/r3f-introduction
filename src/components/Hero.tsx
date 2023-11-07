2f1194d6725cbf7ad3f633249b00e45f19489f72
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2023-11-07 16:15:14 +0900

    :coffin: 不要なコードを削除

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ export const Hero = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-full z-50">
       <div className="mt-[5vh] ml-[5vw]">
-        <h1 className="font-black text-red-500 text-[2.2rem] lg:text-[4.2rem] tracking-tighter overflow-hidden">
+        <h1 className="font-black text-red-500 text-[2.2rem] lg:text-[4.2rem]">
           りんご
         </h1>
       </div>
